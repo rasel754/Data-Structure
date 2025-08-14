be2ae70d2f0648e4eea72a4a1e17be53fcb8accb
unknown <raselahmed73614@gmail.com>
2025-08-14 15:26:31 +0600

    map stl

---
+++ map.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> mp;
+    mp["rasel"] = 5;
+    mp["shoel"] = 9;
+    mp["rana"] = 4;
+    mp["siuuu"] = 8;
+
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << "->" << it->second << endl;
+    }
+
+      // key check
+    if(mp.count("messi"))
+    cout<<" ache "<<endl;
+    else
+    cout<<"nai"<<endl;
+
+    return 0;
+
+  
+}
